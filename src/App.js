50ef2392e3eafdd83dac5b5c48b31bb16ebcb8bd
Sashankr <sashankr97@gmail.com>
2022-03-03 10:38:22 +0530

    setting up basic ui for search game section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='search-section'>
+        <h1>Search For A Game</h1>
+        <input type="text" placeholder='Minecraft...' />
+        <button>Search Game Title</button>
+      </div>
 
+      <div className="deals-section">
+
+      </div>
     </div>
   );
 }
