65cebc651b42389ef000784e12c91416fd6f9200
Sashankr <sashankr97@gmail.com>
2022-03-03 15:02:43 +0530

    making api request to cheap shark

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [gameTitle,setGameTitle] = useState('')
+
+  const searchGame = () => {
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}`)
+    .then((response)=> response.json())
+    .then((data) => console.log(data))
+  }
+
+  const handleInputChange = (event) =>{
+    setGameTitle(event.target.value)
+  }
+
   return (
     <div className="App">
       <div className='search-section'>
         <h1>Search For A Game</h1>
-        <input type="text" placeholder='Minecraft...' />
-        <button>Search Game Title</button>
+        <input type="text" placeholder='Minecraft...' onChange={handleInputChange} value={gameTitle} />
+        <button onClick={searchGame}>Search Game Title</button>
       </div>
 
       <div className="deals-section">
