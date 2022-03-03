fe25d92e79016b74e73f7a26aad71623d1bde9b5
Sashankr <sashankr97@gmail.com>
2022-03-03 15:54:50 +0530

    showing game titles after search

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import './App.css';
 
 function App() {
 
-  const [gameTitle,setGameTitle] = useState('')
+  const [gameTitle, setGameTitle] = useState('')
+  const [searchedGames, setSearchedGames] = useState([]);
 
   const searchGame = () => {
-    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}`)
-    .then((response)=> response.json())
-    .then((data) => console.log(data))
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchedGames(data)
+        console.log(data)
+      })
   }
 
-  const handleInputChange = (event) =>{
+  const handleInputChange = (event) => {
     setGameTitle(event.target.value)
   }
 
@@ -22,7 +26,19 @@ function App() {
         <h1>Search For A Game</h1>
         <input type="text" placeholder='Minecraft...' onChange={handleInputChange} value={gameTitle} />
         <button onClick={searchGame}>Search Game Title</button>
+
+        <div className="games">
+        {searchedGames.map((game,index)=>{
+          return (
+            <div key={index} className="game">
+              {game.external}
+            </div>
+          )
+        })}
       </div>
+      </div>
+
+     
 
       <div className="deals-section">
         <h1>Latest Deals <span role="img" aria-label="fire emoji"> 🔥 </span></h1>
