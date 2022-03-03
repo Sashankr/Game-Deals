caa3db7a06e9e78e7dbc1642d07e0e0020805f86
Sashankr <sashankr97@gmail.com>
2022-03-04 00:29:37 +0530

    deal section api integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const [gameTitle, setGameTitle] = useState('')
   const [searchedGames, setSearchedGames] = useState([]);
-  const [gameDeals,setGameDeals] = useState([])
+  const [gameDeals, setGameDeals] = useState([])
 
   const searchGame = () => {
     fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
@@ -20,14 +20,14 @@ function App() {
     setGameTitle(event.target.value)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=20&pageSize=3`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         setGameDeals(data);
       })
-  },[])
+  }, [])
 
   return (
     <div className="App">
@@ -37,25 +37,31 @@ function App() {
         <button onClick={searchGame}>Search Game Title</button>
 
         <div className="games">
-        {searchedGames.map((game,index)=>{
+          {searchedGames.map((game, index) => {
+            return (
+              <div key={index} className="game">
+                <h4>{game.external}</h4>
+                <img src={game.thumb} alt={game.external} />
+                <small>{game.cheapest}</small>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
+
+
+      <h1>Latest Deals <span role="img" aria-label="fire emoji"> 🔥 </span></h1>
+      <div className="deals-section">
+        {gameDeals.map((game, index) => {
           return (
             <div key={index} className="game">
-              <h4>{game.external}</h4>
-              <img src={game.thumb} alt={game.external} />
-              <small>{game.cheapest}</small>
+              <h3>{game.title}</h3>
+              <img src={game.thumb} alt={game.title} />
+              <p className='normal-price'>{game.normalPrice}</p>
+              <p>{game.salePrice}</p>
+              <h4>You save {game.savings.substring(0,2)}%</h4>
             </div>
-          )
-        })}
-      </div>
-      </div>
-
-     
-
-      <div className="deals-section">
-        <h1>Latest Deals <span role="img" aria-label="fire emoji"> 🔥 </span></h1>
-        {gameDeals.map((game,index)=>{
-          return (
-            <h1>{game.title}</h1>
           )
         })}
       </div>
